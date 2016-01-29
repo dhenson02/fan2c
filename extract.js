464f59081076fdea5a4efab19db699d2dca5ca5b
Deryck Henson <dhenson@mcclatchy.com>
2016-01-28 21:08:50 -0500

    not much other people will be able to use but i'll fix that later

--- extract.js
+++ extract.js
@@ -1,15 +1,17 @@
 'use strict';
 let fs = require('fs');
-
 let filename = process.argv[2];
 let path = __dirname + "/" + filename;
+
+let data = fs.readFileSync(path, {encoding: "utf8"});
+
+let fileObj = JSON.parse(data);
+
+let newObj = fileObj[ filename.replace(".json", "") ] || fileObj;
+
+let newFile = JSON.stringify(newObj);
+
+fs.writeFileSync(path, newFile, { encoding: "utf8" });
+
 console.log(filename, path);
-fs.readFile(path, {encoding: "utf8"}, (err, data) => {
-    let fileObj = JSON.parse(data);
-    console.log("data: ", data);
-    console.log("fileObj: ", fileObj);
-    let newObj = [fileObj[filename.replace(".json", "")]];
-    let newFile = JSON.stringify(newObj);
-    fs.writeFileSync(path, newFile, {encoding: "utf8"});
-    process.exit(0);
-});
+process.exit(0);
